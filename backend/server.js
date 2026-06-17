72e63bae7672016d3eb0fc5fe425d86ff9f51d49
Sheshanth <sheshanthsaravanan@gmail.com>
2026-06-17 11:56:13 +0530

    Backend code updated

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(
 
 app.use(express.json());
 
-app.use("/api/atm", require("./routes/atmRoutes"));
+app.use("/api/atm", require("./routes/atmroutes"));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
